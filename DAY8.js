c6770780e80452e4a578e226bb833de6f0004b64
Baivab Bhusal <baivabbhusal229@gmail.com>
2025-03-02 19:40:19 +0545

    Day 8 file added

---
+++ DAY8.js
@@ -0,0 +1,12 @@
+//Activity 1--Template literals
+
+const userInfo={
+    name:"bhupu",
+    age:23
+};
+//string including name and age
+console.log(`My name is ${userInfo.name}.I am ${userInfo.age} years old.`)
+
+//multiline string
+console.log(`I am 
+    good wbu.`);
